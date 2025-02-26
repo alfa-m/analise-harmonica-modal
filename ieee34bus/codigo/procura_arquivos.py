5eae4d1dddece21dca156f0bb6cbfbab13880240
Alfa Marine <alfamarine.ferreira@gmail.com>
2025-02-26 12:45:56 -0300

    remove arquivos da pasta ieee34bus (que na vdd era só pra renomear mas o git não tá me ajudando)
    demais arquivos da pasta

--- ieee34bus/codigo/procura_arquivos.py
+++
@@ -1,9 +0,0 @@
-import os, fnmatch
-
-def procura(padrao, caminho):
-    resultado = []
-    for raiz, diretorios, arquivos in os.walk(caminho):
-        for nome in arquivos:
-            if fnmatch.fnmatch(nome, padrao):
-                resultado.append(nome)
-    return resultado
