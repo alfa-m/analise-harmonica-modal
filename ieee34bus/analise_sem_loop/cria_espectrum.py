73f41cd135286365fc4c5bcd0a2ee6c95018994c
Alfa Marine <alfamarine.ferreira@gmail.com>
2025-02-26 12:31:53 -0300

    remove arquivos da pasta ieee34bus (que na vdd era só pra renomear mas o git não tá me ajudando)
    arquivos da analise sem loop

--- ieee34bus/analise_sem_loop/cria_espectrum.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-harmonico_pd = pd.DataFrame()
-harmonico_pd['harmonico'] = np.arange(1,100.001,(0.5/60)).tolist()
-harmonico_pd['harmonico'] = harmonico_pd['harmonico'].apply(lambda h : round(h, 6))
-harmonico_pd['amplitude'] = 100
-harmonico_pd['angulo'] = 0
-harmonico_pd.to_csv("espectro_harmonico_longo.csv", index=False, header=False)
